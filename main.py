48af1fca01cdbc30b8501a19328516aa4626b134
Omar Ansari <StreetFighterLan123@gmail.com>
2020-06-22 13:40:18 -0500

    Added accuracy (total).

--- main.py
+++ main.py
@@ -13,24 +13,31 @@ divs = soup.find_all("div")
 
 string = ""
 my_variable_names_suck = ""
+bruh_momento = ""
 for div in divs:
     if "PP" in div.text:
-        ppTD = soup.find_all('td')[1]
         rankTD = soup.find_all('td')[0]
+        ppTD = soup.find_all('td')[1]
+        accuracyTD = soup.find_all('td')[2]
         for diarrhea in ppTD:
             if "PP" not in diarrhea:
                 string += diarrhea
         for pebble in rankTD:
             if "Rank" not in pebble:
                 my_variable_names_suck += pebble
+        for cookiezi in accuracyTD:
+            if "Accuracy" not in cookiezi:
+                bruh_momento += cookiezi
         #print(string)
 
 ppList = string.split()
 pp = float(ppList[0])
+formatted_pp = "{:,}".format(pp)
 
 rankList = my_variable_names_suck.split()
 rank = rankList[0]
-formatted_pp = "{:,}".format(pp)
 
-print(f'\nPP: {formatted_pp} \nRank: {rank}')
+accuracyList = bruh_momento.split()
+accuracy = accuracyList[0]
+print(f'\nPP: {formatted_pp} \nRank: {rank}\nAccuracy: {accuracy}')
 
