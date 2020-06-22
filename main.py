296bf5feb62a3c5667753cc0aaaf21d7844f7a3d
Omar Ansari <StreetFighterLan123@gmail.com>
2020-06-22 13:28:27 -0500

    Added rank.

--- main.py
+++ main.py
@@ -14,19 +14,27 @@ mydivs = soup.find_all("div")
 
 ppdivs = []
 string = ""
+my_variable_names_suck = ""
 for div in mydivs:
     if "PP" in div.text:
         ppdivs.append(div.text)
-        poopoo_thing = soup.find_all('td')[1]
+        ppTD = soup.find_all('td')[1]
+        rankTD = soup.find_all('td')[0]
         #print(poopoo_thing)
-        for diarrhea in poopoo_thing:
+        for diarrhea in ppTD:
             if "PP" not in diarrhea:
                 string += diarrhea
+        for pebble in rankTD:
+            if "Rank" not in pebble:
+                my_variable_names_suck += pebble
         #print(string)
 
 ppList = string.split()
 pp = float(ppList[0])
 
-y = "{:,}".format(pp)
-print(f'PP: {y}')
+rankList = my_variable_names_suck.split()
+rank = rankList[0]
+formatted_pp = "{:,}".format(pp)
+
+print(f'\nPP: {formatted_pp} \nRank: {rank}')
 
