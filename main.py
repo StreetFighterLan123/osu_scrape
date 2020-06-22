27c788197c0477dff3225e1308b7813a95998ee1
Omar Ansari <StreetFighterLan123@gmail.com>
2020-06-22 11:34:15 -0500

    added the project

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+raw = input("User: ").lower().strip()
+
+result = requests.get(f'https://ameobea.me/osutrack/user/{raw}/')
+
+src = result.content
+
+soup = BeautifulSoup(src, "lxml")
+
+
+mydivs = soup.find_all("div")
+
+ppdivs = []
+string = ""
+for div in mydivs:
+    if "PP" in div.text:
+        ppdivs.append(div.text)
+        poopoo_thing = soup.find_all('td')[1]
+        #print(poopoo_thing)
+        for diarrhea in poopoo_thing:
+            if "PP" not in diarrhea:
+                string += diarrhea
+        #print(string)
+
+ppList = string.split()
+pp = float(ppList[0])
+
+print("{:,}".format(pp))
+
+
