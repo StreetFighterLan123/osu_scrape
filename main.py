0a849b4767c84caa9abff6199a6b54d635d423b1
Omar Ansari <StreetFighterLan123@gmail.com>
2020-06-28 17:54:55 -0500

    Fixed something.

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-import requests
+import requests, time
 from bs4 import BeautifulSoup
+running = True
 
-raw = input("Player: ").lower().strip()
+while running:
 
-result = requests.get(f'https://ameobea.me/osutrack/user/{raw}/')
+    raw = input("Player: ").lower().strip()
 
-src = result.content
+    result = requests.get(f'https://ameobea.me/osutrack/user/{raw}/')
 
-soup = BeautifulSoup(src, "lxml")
+    src = result.content
 
-divs = soup.find_all("div")
+    soup = BeautifulSoup(src, "lxml")
 
-string = ""
-my_variable_names_suck = ""
-bruh_momento = ""
-for div in divs:
-    if "PP" in div.text:
-        rankTD = soup.find_all('td')[0]
-        ppTD = soup.find_all('td')[1]
-        accuracyTD = soup.find_all('td')[2]
-        for diarrhea in ppTD:
-            if "PP" not in diarrhea:
-                string += diarrhea
-        for pebble in rankTD:
-            if "Rank" not in pebble:
-                my_variable_names_suck += pebble
-        for rrtyui in accuracyTD:
-            if "Accuracy" not in rrtyui:
-                bruh_momento += rrtyui
+    divs = soup.find_all("div")
+
+    string = ""
+    my_variable_names_suck = ""
+    bruh_momento = ""
+    for div in divs:
+        if "PP" in div.text:
+            rankTD = soup.find_all('td')[0]
+            ppTD = soup.find_all('td')[1]
+            accuracyTD = soup.find_all('td')[2]
+            for diarrhea in ppTD:
+                if "PP" not in diarrhea:
+                    string += diarrhea
+            for pebble in rankTD:
+                if "Rank" not in pebble:
+                    my_variable_names_suck += pebble
+            for rrtyui in accuracyTD:
+                if "Accuracy" not in rrtyui:
+                    bruh_momento += rrtyui
         #print(string)
 
-ppList = string.split()
-pp = float(ppList[0])
-formatted_pp = "{:,}".format(pp)
+    ppList = string.split()
+    pp = float(ppList[0])
+    formatted_pp = "{:,}".format(pp)
 
-rankList = my_variable_names_suck.split()
-rank = rankList[0]
+    rankList = my_variable_names_suck.split()
+    rank = rankList[0]
 
-accuracyList = bruh_momento.split()
-accuracy = accuracyList[0]
-print(f'\nPP: {formatted_pp} \nRank: {rank}\nAccuracy: {accuracy}')
-
+    accuracyList = bruh_momento.split()
+    accuracy = accuracyList[0]
+    print(f'\nPP: {formatted_pp} \nRank: {rank}\nAccuracy: {accuracy}')
+    print("\n")
+    kill = input("Quit? (y/n)")
+    killf = kill.lower().strip()
+    if killf == "yes" or killf == "y":
+        running = False
+        exit()
+    else:
+        time.sleep(1)
